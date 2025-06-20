430f8993c324d245671c97f0be6ce8e6be374ee3
eslamahmed74 <islamaboelmaged74@gmail.com>
2025-06-21 02:56:08 +0300

    create the adsRequestCrud

--- src/advertisement-request/advertisement-request.module.ts
+++ src/advertisement-request/advertisement-request.module.ts
@@ -1,11 +1,14 @@
-import { Module } from '@nestjs/common';
-import { AdvertisementRequestController } from './advertisement-request.controller';
-import { AdvertisementRequestService } from './advertisement-request.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AdvertisementRequest } from 'src/entities/advertisement-request.entity';
+import { Module } from "@nestjs/common";
+import { AdvertisementRequestController } from "./advertisement-request.controller";
+import { AdvertisementRequestService } from "./advertisement-request.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AdvertisementRequest } from "src/entities/advertisement-request.entity";
+import { CustomJwtService } from "src/common/services/custom-jwt.service";
+import { CommonModule } from "src/common/common.module";
+
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AdvertisementRequest])],
+  imports: [TypeOrmModule.forFeature([AdvertisementRequest]), CommonModule],
   controllers: [AdvertisementRequestController],
   providers: [AdvertisementRequestService],
 })
