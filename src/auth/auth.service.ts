c565f90e5a14bcc862509ef68d6648b1bfaf2986
eslamahmed74 <islamaboelmaged74@gmail.com>
2025-06-21 01:01:29 +0300

    fix the object type problem

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,51 +2,59 @@ import {
   BadRequestException,
   Injectable,
   UnauthorizedException,
-} from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { CustomJwtService } from 'src/common/services/custom-jwt.service';
-import { User } from 'src/entities/user.entity';
-import { Repository } from 'typeorm';
-import { SignUpDto } from './dtos/signup.dto';
-import { SignInDto } from './dtos/signin.dto';
+} from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { status, User, VerificationStatus } from "src/entities/user.entity";
+import { CustomJwtService } from "src/common/services/custom-jwt.service";
+import { SignUpDto } from "./dtos/signup.dto";
+import { SignInDto } from "./dtos/signin.dto";
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User) private userRepo: Repository<User>,
-    private jwtService: CustomJwtService,
+    private jwtService: CustomJwtService
   ) {}
 
   async signUp(data: SignUpDto) {
-    const isUserExsite = await this.userRepo.findOne({
+    const isUserExist = await this.userRepo.findOne({
       where: { email: data.email },
     });
-    if (isUserExsite) {
-      throw new BadRequestException('invalid credentail');
+    if (isUserExist) {
+      throw new BadRequestException(
+        "Invalid credentials - email already used."
+      );
     }
 
-    const user = await this.userRepo.create({
+    const user = this.userRepo.create({
       fullName: data.fullName,
       email: data.email,
       phone: data.phone,
       password: data.password,
-      role: data.role,
-      profilePhotoUrl: '',
-      idCardUrl: '',
-      isIdVerified: true,
-      workIdUrl: '',
-      isWorkIdVerified: true,
+      //لما تيجي تكمل غير الادمن دي 
+      role: status.ADMIN,
+      profilePhotoUrl: "",
+      idFrontCardUrl: {},
+      idBackCardUrl: {},
+      isIdVerified: false,
+      workIdUrl: "",
+      isWorkIdVerified: VerificationStatus.PENDING,
+      subscriptionStatus: false,
+      subscriptionType: null,
+      otpCode: null,
+      otpExpiresAt: null,
     });
+
+    await this.userRepo.save(user);
 
     const token = this.jwtService.generateToken({
       id: user.id,
       role: user.role,
     });
 
-    await this.userRepo.save(user);
-
     return {
-      message: 'Signup successful',
+      message: "Signup successful",
       user: {
         id: user.id,
         fullName: user.fullName,
@@ -58,10 +66,12 @@ export class AuthService {
   }
 
   async signIn(data: SignInDto) {
-    const user = await this.userRepo.findOne({ where: { email: data.email } });
+    const user = await this.userRepo.findOne({
+      where: { email: data.email },
+    });
 
     if (!user || user.password !== data.password) {
-      throw new UnauthorizedException('Invalid email or password');
+      throw new UnauthorizedException("Invalid email or password");
     }
 
     const token = this.jwtService.generateToken({
@@ -70,7 +80,7 @@ export class AuthService {
     });
 
     return {
-      message: 'Signin successful',
+      message: "Signin successful",
       user: {
         id: user.id,
         fullName: user.fullName,
